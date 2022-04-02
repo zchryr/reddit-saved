1f136125ffdfdcd4a7be9fb956a90fef75da8be2
Zachary Rohrbach <zachary@rohrbach.tech>
2022-04-01 23:01:44 -0400

    Codacy.

--- scraper/mongo.py
+++ scraper/mongo.py
@@ -1,13 +1,14 @@
 import pymongo
 
 class mongoClient:
-    def __init__(self, connectionURL, protocol, port, username, password, name, ssl, tlsCAFile, redditUsername) -> None:
+    def __init__(self, connectionURL, protocol, port, 
+        username, password, name, ssl, tlsCAFile, redditUsername) -> None:
         self.client = pymongo.MongoClient(host=protocol + "://" + connectionURL,
-                                          port=port,
-                                          username=username,
-                                          password=password,
-                                          ssl=ssl,
-                                          tlsCAFile=tlsCAFile)
+            port=port,
+            username=username,
+            password=password,
+            ssl=ssl,
+            tlsCAFile=tlsCAFile)
         self.db = self.client[name]
         colName = 'saves - ' + redditUsername
         self.col = self.db[colName]
